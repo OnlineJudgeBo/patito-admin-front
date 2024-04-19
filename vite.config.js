26c3afbebd7f718f759f06caf5f5ce08414c8805
Samuel Loza <samuel.loza26@gmail.com>
2024-04-19 18:09:01 -0400

    Change admin url link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/admin2/',
+  base: '/admin/',
   plugins: [react()],
   resolve: {
     alias: {
