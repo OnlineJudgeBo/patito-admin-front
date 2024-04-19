8ae619f82f85b50097e63ea16f4b884344909eb2
Samuel Loza <samuel.loza26@gmail.com>
2024-04-19 09:02:58 -0400

    change admin by admin2

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import UseAuth from './hooks/UseAuth';
 
 const PrivateRoute = ({ children }) => {
     const { isAuthenticated, isLoading } = UseAuth();
-    const authRedirectUrl = process.env.REACT_APP_AUTH_REDIRECT_URL;
+    const authRedirectUrl = process.env.VITE_REACT_APP_AUTH_REDIRECT_URL;
 
     if (isLoading) {
         return <div>Cargando...</div>;
