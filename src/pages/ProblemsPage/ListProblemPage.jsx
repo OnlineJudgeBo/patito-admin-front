8b85112185e5d30109517fd273aaeddf2995661f
Samuel Loza <samuel.loza26@gmail.com>
2024-04-21 14:48:33 -0400

    Remove open in new page

--- src/pages/ProblemsPage/ListProblemPage.jsx
+++ src/pages/ProblemsPage/ListProblemPage.jsx
@@ -103,7 +103,6 @@ function ListProblemsPage() {
                                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{problem.problemId}</td>
                                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         <a className="text-blue-600 hover:text-blue-900"
-                                            target="_blank"
                                             href={`/admin/problems/edit/${problem.problemId}`}>
                                             {problem.title}
                                         </a>
