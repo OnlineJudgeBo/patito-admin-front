e9d44f46c92b5302fd05d45348fbe6ec4fbb5827
Samuel Loza Ramirez <starsaminf@gmail.com>
2025-02-10 11:00:36 -0400

    Fix logout and remove logout logs

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,14 +1,28 @@
 import Cookies from 'js-cookie';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-const CLIENT_URL = import.meta.env.VITE_REACT_APP_AUTH_REDIRECT_URL;
+
+const redirectLogoutURL = () => {
+    const hostname = window.location.hostname;
+    let url = "";
+    if (hostname === "juezvirtual.local") {
+        url = "https://juezvirtual.local/oj/logout.php";
+    } else if (hostname === "juezvirtual.local") {
+        url = "https://juezvirtual.local/logout.php";
+    } else if (hostname === "jv.umsa.bo") {
+        url = "http://jv.umsa.bo/logout.php";
+    } else {
+        url = "/logout.php";
+    }
+    return url;
+};
 
 export default function LogoutPage() {
     const navigate = useNavigate();
 
     useEffect(() => {
         Cookies.remove('accessToken', { path: '/' });
-        window.location.href = CLIENT_URL + "/logout.php";
+        window.location.href = redirectLogoutURL();
     }, [navigate]);
 
     return <div>Cerrando sesión...</div>;
