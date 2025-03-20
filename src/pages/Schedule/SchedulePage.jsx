b319bd8f2696451df587f4bca0b6c0d739d096ff
Samuel Loza <samuel.loza26@gmail.com>
2025-03-20 18:28:11 -0400

    Add schedule feature

---
+++ src/pages/Schedule/SchedulePage.jsx
@@ -0,0 +1,12 @@
+
+import { AddSubjectPage } from "./AddSubjetPage"
+
+function SchedulePage() {
+    return (
+        <div>
+            <AddSubjectPage></AddSubjectPage>
+        </div>
+    );
+}
+
+export default SchedulePage;
