cb536d1537c5a4ce33d9f436ebd6d86ab7dac480
Samuel Loza <samuel.loza26@gmail.com>
2025-03-20 19:49:06 -0400

    Fix styles

--- src/pages/TopicsClassificationsPage/TopicsClassificationsPage.jsx
+++ src/pages/TopicsClassificationsPage/TopicsClassificationsPage.jsx
@@ -1,5 +1,5 @@
 import { useToast } from "@/components/ui/use-toast";
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import TopicCard from "../../components/TopicCard/TopicCard";
 import { apiService } from "../../services/apiService";
 import { AddTopicComponent } from "./AddTopicComponent";
@@ -33,7 +33,7 @@ function TopicsClassificationsPage() {
 
 
     return (
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto p-4 w-full min-w-full">
             <h1 className="text-2xl font-bold mb-4">Temas</h1>
             <AddTopicComponent></AddTopicComponent>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
